182892ff9a7be91cd93752f04f2d57f05b573939
Mark van Huijstee <Mark.van.Huijstee@de-it-krachten.nl>
2022-11-01 09:42:27 +0100

    Move to include.sh style for external import

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dir=../../git-tools/cicd/docker
-
-rsync -c -av $dir/docker-init.sh  files/
-rsync -c -av $dir/docker-build.sh files/
-rsync -c -av $dir/ansible         files/
-rsync -c -av $dir/templates       files/
-
